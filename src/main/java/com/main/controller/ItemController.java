9350219732f1ec6e909339f54264b49c24440763
Amol Tukaram Suryavanshi <AS00587213@INPUSCPC10460.TechMahindra.com>
2019-01-02 15:57:49 +0530

    maste

--- src/main/java/com/main/controller/ItemController.java
+++ src/main/java/com/main/controller/ItemController.java
@@ -25,7 +25,6 @@ import com.main.services.ItemServicesImpl;
 import com.main.util.ItemNotFoundException;
 
 @RestController
-@RequestMapping("/item-services")
 public class ItemController {
 	
 	private ItemServicesImpl itemServicesImpl;
